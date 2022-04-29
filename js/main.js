b34f297b34162a2fdc22327fe73f22a29621acba
tiagoferreira18 <tiagoferreira18@gmail.com>
2022-04-29 20:11:37 -0300

    Finalizando

--- js/main.js
+++ js/main.js
@@ -17,13 +17,13 @@ for(let link of links){
 
 }
 
-/* Função para mudar o header da pagina */
-function changeHeaderWhenScroll(){
-
 /*  Mudar o header da página quando der scroll */
 const header = document.querySelector('#header');
 /* Objeto header com a propriedade offsetHeight */
 const navHeight = header.offsetHeight;
+/* Função para mudar o header da pagina */
+function changeHeaderWhenScroll(){
+
 
 if(window.scrollY >=navHeight){
     //scroll maior que a altura do header
@@ -46,7 +46,13 @@ const swiper = new Swiper('.swiper', {
         
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 });
 
 /* scrollReveal: Mostrar elementos quando der scroll na página */
@@ -70,9 +76,40 @@ const swiper = new Swiper('.swiper', {
 //     `, { interval: 100})
 
 
+
+/* Menu ativo conforme a seção visivel na página */
+
+/* Todas as seções que possuem um id */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSec(){
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for(const section of sections){
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStar = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint<= sectionTop + sectionHeight
+
+        if(checkpointStar && checkpointEnd){
+            document.querySelector('nav ul li a[href*=' + sectionId +']').classList.add('active')
+        }else{
+            document.querySelector('nav ul li a[href*=' + sectionId +']').classList.remove('active')
+        }
+
+
+    }
+
+
+}
+
+
     /* when scrool */
 
     window.addEventListener('scroll', ()=>{
         changeHeaderWhenScroll()
+        activateMenuAtCurrentSec()
     })
 
